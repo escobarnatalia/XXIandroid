f6a03c42ae81160b1245bff32578ee684ab79703
Wilson <wilsonst.suarez@hotmail.com>
2020-12-08 16:21:59 -0500

    Init Screens

--- app/src/main/java/co/natalia/xxiandroid/LogSign.java
+++ app/src/main/java/co/natalia/xxiandroid/LogSign.java
@@ -6,43 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LogSign extends AppCompatActivity {
 
-    private Button next;
-
-
+    private Button next2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_sign);
 
-        next=findViewById(R.id.next);
+        next2=findViewById(R.id.next2);
 
 
-        next.setOnClickListener(
+
+        next2.setOnClickListener(
                 (v) -> {
-                    Intent i = new Intent(this,LogSign.class);
+                    Intent i = new Intent(this,Home.class);
                     startActivity(i);
 
                 }
-
-
-
 
 
         );
 
 
 
-
     }
-
-
-
-
-
-
-
-
 }
