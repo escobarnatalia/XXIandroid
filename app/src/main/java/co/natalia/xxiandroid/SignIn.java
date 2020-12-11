b16bbfdaa2f93a07474f996eeba98dc776927bf7
Wilson <wilsonst.suarez@hotmail.com>
2020-12-11 15:18:55 -0500

    Send data to firebase READY

--- app/src/main/java/co/natalia/xxiandroid/SignIn.java
+++ app/src/main/java/co/natalia/xxiandroid/SignIn.java
@@ -4,42 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class SignIn extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.UUID;
+
+public class SignIn extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseDatabase db;
+    private EditText username,email2,pass2,pass2c;
     private Button signinbtn;
-    private EditText username;
-    private EditText email2;
-    private EditText pass2;
-    private EditText pass2c;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        signinbtn=findViewById(R.id.signinbtn);
+        db=FirebaseDatabase.getInstance();
 
         username=findViewById(R.id.username);
         email2=findViewById(R.id.email2);
         pass2=findViewById(R.id.pass2);
         pass2c=findViewById(R.id.pass2c);
+        signinbtn=findViewById(R.id.signinbtn);
 
+        signinbtn.setOnClickListener(this);
 
-        signinbtn.setOnClickListener(
-                (v)->{
-                    Intent i = new Intent(this,Home.class);
-                    startActivity(i);
-                }
-
-
-        );
+                    /*Intent i = new Intent(this,Home.class);
+                    startActivity(i);*/
 
 
 
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.signinbtn:
+                String id= db.getReference().child("androidUser").push().getKey();
+                DatabaseReference reference =db.getReference().child("androidUser").child(id);
+                AndroidUsers androidusers = new AndroidUsers(
+                        id,
+                        username.getText().toString(),
+                        email2.getText().toString(),
+                        pass2.getText().toString(),
+                        pass2c.getText().toString()
+                );
+
+                reference.setValue(androidusers);
+
+
+                break;
+
+
+
+        }
     }
 }
