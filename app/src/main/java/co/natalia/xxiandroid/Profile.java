aa63c64711bcd1ce31ee023853d91c7124e6735a
escobarnatalia <60725004+escobarnatalia@users.noreply.github.com>
2020-12-14 14:24:30 -0500

    Profile

--- app/src/main/java/co/natalia/xxiandroid/Profile.java
+++ app/src/main/java/co/natalia/xxiandroid/Profile.java
@@ -1,14 +1,60 @@
 package co.natalia.xxiandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Profile extends AppCompatActivity {
+
+    private TextView usernameTx;
+    private Button SignOffBtn;
+    private FirebaseAuth auth;
+    private FirebaseDatabase db;
+    private AndroidUsers users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        usernameTx = findViewById(R.id.usernameTx);
+        SignOffBtn = findViewById(R.id.SignOffBtn);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+
+        recoverUser();
+    }
+
+    private void recoverUser() {
+        if (auth.getCurrentUser() != null){
+            String id = auth.getCurrentUser().getUid();
+            db.getReference().child("androidUser").child(id).addListenerForSingleValueEvent(
+                    new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot snapshot) {
+                            users = snapshot.getValue(AndroidUsers.class);
+                            usernameTx.setText(users.getUsername());
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    }
+            );
+
+
+        }
     }
 }
